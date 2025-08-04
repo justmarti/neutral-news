799da1a4b8f93b0525579112f12da735ac45865a
Martí Espinosa <martiespinosa99@gmail.com>
2025-08-04 20:25:16 +0200

    fix: improve color contrast for light/dark modes

--- NeutralNews/Utils/ColorExtensions.swift
+++ NeutralNews/Utils/ColorExtensions.swift
@@ -26,8 +26,45 @@ extension Color {
         return luminance > 0.5 ? .black : .white
     }
     
+    /// Calculates the contrast ratio between two colors using WCAG formula
+    fileprivate func contrastRatio(with otherColor: Color) -> CGFloat {
+        let luminance1 = self.relativeLuminance()
+        let luminance2 = otherColor.relativeLuminance()
+        
+        let lighter = max(luminance1, luminance2)
+        let darker = min(luminance1, luminance2)
+        
+        return (lighter + 0.05) / (darker + 0.05)
+    }
+    
+    /// Calculates the relative luminance of the color
+    fileprivate func relativeLuminance() -> CGFloat {
+        let uiColor = UIColor(self)
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        var alpha: CGFloat = 0
+        
+        uiColor.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+        
+        // Apply gamma correction
+        let sRGBToLinear = { (component: CGFloat) -> CGFloat in
+            if component <= 0.03928 {
+                return component / 12.92
+            } else {
+                return pow((component + 0.055) / 1.055, 2.4)
+            }
+        }
+        
+        let linearR = sRGBToLinear(red)
+        let linearG = sRGBToLinear(green)
+        let linearB = sRGBToLinear(blue)
+        
+        return 0.2126 * linearR + 0.7152 * linearG + 0.0722 * linearB
+    }
+    
     /// Returns a View with adaptive background color based on current color scheme
-    /// Light mode: makes the color lighter, Dark mode: makes the color darker
+    /// Dynamically adjusts brightness to maximize contrast with text
     var adaptiveBackground: some View {
         AdaptiveBackgroundView(baseColor: self)
     }
@@ -38,6 +75,39 @@ struct AdaptiveBackgroundView: View {
     @Environment(\.colorScheme) private var colorScheme
     
     var body: some View {
+        let targetTextColor: Color = colorScheme == .light ? .black : .white
+        let adaptedColor = optimizeContrastDynamically(for: targetTextColor)
+        
+        return adaptedColor
+    }
+    
+    private func optimizeContrastDynamically(for textColor: Color) -> Color {
+        let currentLuminance = baseColor.relativeLuminance()
+        let textLuminance = textColor.relativeLuminance()
+        let targetContrast: CGFloat = 4.5
+        let currentContrast = baseColor.contrastRatio(with: textColor)
+        
+        if currentContrast >= targetContrast {
+            if textLuminance < 0.5 {
+                let minLightModeTarget: CGFloat = 0.5
+                if currentLuminance >= minLightModeTarget {
+                    return baseColor
+                }
+            } else {
+                let maxDarkModeTarget: CGFloat = 0.3
+                if currentLuminance <= maxDarkModeTarget {
+                    return baseColor
+                }
+            }
+        }
+        
+        let targetLuminance: CGFloat
+        if textLuminance < 0.5 {
+            targetLuminance = max(0.6, min(1, (textLuminance + 0.05) * targetContrast - 0.05))
+        } else {
+            targetLuminance = max(0, min(1, (textLuminance + 0.05) / targetContrast - 0.05))
+        }
+        
         let uiColor = UIColor(baseColor)
         var red: CGFloat = 0
         var green: CGFloat = 0
@@ -46,28 +116,12 @@ struct AdaptiveBackgroundView: View {
         
         uiColor.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
         
-        let adaptedColor: Color
-        switch colorScheme {
-        case .light:
-            // Make color lighter by blending with white
-            let lightFactor: CGFloat = 0.3
-            adaptedColor = Color(
-                red: red + (1.0 - red) * lightFactor,
-                green: green + (1.0 - green) * lightFactor,
-                blue: blue + (1.0 - blue) * lightFactor
-            )
-        case .dark:
-            // Make color darker by reducing brightness
-            let darkFactor: CGFloat = 0.6
-            adaptedColor = Color(
-                red: red * darkFactor,
-                green: green * darkFactor,
-                blue: blue * darkFactor
-            )
-        @unknown default:
-            adaptedColor = baseColor
-        }
+        let adjustmentFactor = sqrt(targetLuminance / currentLuminance)
         
-        return adaptedColor
+        return Color(
+            red: min(1.0, max(0.0, red * adjustmentFactor)),
+            green: min(1.0, max(0.0, green * adjustmentFactor)),
+            blue: min(1.0, max(0.0, blue * adjustmentFactor))
+        )
     }
 }
