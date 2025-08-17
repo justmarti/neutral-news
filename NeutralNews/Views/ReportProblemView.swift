d5138b53ed3e3c49cf942d76fe6d4525e83f527d
Martí Espinosa <martiespinosa99@gmail.com>
2025-08-17 01:55:15 -0400

    chore: update project build settings

---
+++ NeutralNews/Views/ReportProblemView.swift
@@ -0,0 +1,18 @@
+//
+//  ReportProblemView.swift
+//  NeutralNews
+//
+//  Created by Martí Espinosa Farran on 10/8/25.
+//
+
+import SwiftUI
+
+struct ReportProblemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReportProblemView()
+}
